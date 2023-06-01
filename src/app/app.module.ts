c5f0ec0c73e1f18b1f9455d937fc88e6e65163c8
adamspicedev <adam@spicey.dev>
2023-06-01 17:03:04 +1200

    added firestore config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
+import { environment } from 'src/environments/environment.development';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -9,7 +12,17 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [AppComponent, NavComponent, DashboardComponent],
-  imports: [BrowserModule, NgbModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    AppRoutingModule,
+    provideFirebaseApp(() =>
+      initializeApp({
+        ...environment.firebase,
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/assets/scripts/setEnv.ts
+++ src/assets/scripts/setEnv.ts
@@ -43,12 +43,14 @@ const environmentFileContent = `
 
   export const environment = {
     production: ${isProduction},
-    FIREBASE_API_KEY: '${process.env.FIREBASE_API_KEY}',
-    FIREBASE_AUTH_DOMAIN: '${process.env.FIREBASE_AUTH_DOMAIN}',
-    FIREBASE_PROJECT_ID: '${process.env.FIREBASE_PROJECT_ID}',
-    FIREBASE_STORAGE_BUCKET: '${process.env.FIREBASE_STORAGE_BUCKET}',
-    FIREBASE_MESSAGING_ID: '${process.env.FIREBASE_MESSAGING_ID}',
-    FIREBASE_APP_ID: '${process.env.FIREBASE_APP_ID}',
+    firebase: {
+      FIREBASE_API_KEY: '${process.env.FIREBASE_API_KEY}',
+      FIREBASE_AUTH_DOMAIN: '${process.env.FIREBASE_AUTH_DOMAIN}',
+      FIREBASE_PROJECT_ID: '${process.env.FIREBASE_PROJECT_ID}',
+      FIREBASE_STORAGE_BUCKET: '${process.env.FIREBASE_STORAGE_BUCKET}',
+      FIREBASE_MESSAGING_ID: '${process.env.FIREBASE_MESSAGING_ID}',
+      FIREBASE_APP_ID: '${process.env.FIREBASE_APP_ID}',
+    },
   };
 `;
 
