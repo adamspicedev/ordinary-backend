fa646bd649c94eb8e03ba758378c75f0951674d1
adamspicedev <adam@spicey.dev>
2023-06-01 17:20:11 +1200

    fixed deployment issue

--- src/assets/scripts/setEnv.ts
+++ src/assets/scripts/setEnv.ts
@@ -26,16 +26,13 @@ if (!existsSync(envDirectory)) {
 }
 
 //creates the `environment.prod.ts` and `environment.ts` file if it does not exist
-writeFileUsingFS('./src/environments/environment.development.ts', '');
 writeFileUsingFS('./src/environments/environment.ts', '');
 
 // Checks whether command line argument of `prod` was provided signifying production mode
 const isProduction = environment === 'prod';
 
 // choose the correct targetPath based on the environment chosen
-const targetPath = isProduction
-  ? './src/environments/environment.ts'
-  : './src/environments/environment.development.ts';
+const targetPath = './src/environments/environment.ts';
 
 //actual content to be compiled dynamically and pasted into respective environment files
 const environmentFileContent = `
@@ -44,12 +41,12 @@ const environmentFileContent = `
   export const environment = {
     production: ${isProduction},
     firebase: {
-      FIREBASE_API_KEY: '${process.env.FIREBASE_API_KEY}',
-      FIREBASE_AUTH_DOMAIN: '${process.env.FIREBASE_AUTH_DOMAIN}',
-      FIREBASE_PROJECT_ID: '${process.env.FIREBASE_PROJECT_ID}',
-      FIREBASE_STORAGE_BUCKET: '${process.env.FIREBASE_STORAGE_BUCKET}',
-      FIREBASE_MESSAGING_ID: '${process.env.FIREBASE_MESSAGING_ID}',
-      FIREBASE_APP_ID: '${process.env.FIREBASE_APP_ID}',
+      apiKey: '${process.env.FIREBASE_API_KEY}',
+      authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
+      projectId: '${process.env.FIREBASE_PROJECT_ID}',
+      storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
+      messagingSenderId: '${process.env.FIREBASE_MESSAGING_ID}',
+      appId: '${process.env.FIREBASE_APP_ID}',
     },
   };
 `;
